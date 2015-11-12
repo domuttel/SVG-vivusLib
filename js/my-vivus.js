d29263663be74f765ce5710b070dbb7d845253d8
Dominic Muttel <dominic.muttel@gmail.com>
2015-11-12 11:56:31 -0700

    added svg and cleaned up

--- js/my-vivus.js
+++ js/my-vivus.js
@@ -1,28 +1,24 @@
-
-
-// function easeOutBounce (x) {
-//         var base = -Math.cos(x * (0.5 * Math.PI)) + 1;
-//         var rate = Math.pow(base,1.5);
-//         var rateR = Math.pow(1 - x, 2);
-//         var progress = -Math.abs(Math.cos(rate * (2.5 * Math.PI) )) + 1;
-//         return (1- rateR) + (progress * rateR);
-//       }
-
-      // var timing,
-      //   timingProps = {
-      //     type: 'delayed',
-      //     duration: 150,
-      //     start: 'autostart',
-      //     pathTimingFunction: Vivus.LINEAR,
-      //     animTimingFunction: Vivus.LINEAR
-      //   };
-
-      var dominic = new Vivus('Layer_1', {
-        type: 'async', //mapping
+      var dominic = new Vivus('firstname', {
+        type: 'delayed', //mapping
         pathTimingFunction: Vivus.EASE_OUT_BOUNCE,
-        duration: 200,
+        duration: 150,
+        delay: 1,
         start: 'autostart',
-        dashGap: 60,
+        dashGap: 5,
+        forceRender: false},
+
+        function () {
+          if (window.console) {
+            console.log('Animation finished. [log triggered from callback]');
+          }
+        });
+
+      var dominic = new Vivus('chevron', {
+        type: 'async', //mapping
+        pathTimingFunction: Vivus.EASE_IN,
+        duration: 100,
+        start: 'autostart',
+        dashGap: 10,
         forceRender: false},
 
         function () {
